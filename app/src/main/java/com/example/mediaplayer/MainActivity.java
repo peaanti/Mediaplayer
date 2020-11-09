53bf4ebb23c9a610e95a8b8079b25eae9b13732f
student2 <matmir04@yandex.ru>
2020-11-09 20:40:09 +0300

    сворачивание приложения (ошибка если музыка уже играет)

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -6,21 +6,38 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private MediaPlayer mp;
+    private SeekBar sb;
     ImageButton imageButton;
     boolean flag = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mp = MediaPlayer.create(this, R.raw.song);
         imageButton = findViewById(R.id.bt1);
+
+        sb = findViewById(R.id.seekBar);
+
+        sb.setOnSeekBarChangeListener(this);
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp.pause();
+        playStop(null);
     }
 
     boolean is_playing = false;
-    public void PlayStop(View v){
+    public void playStop(View v){
         mp.start();
         if (is_playing) {
             mp.pause();
@@ -42,4 +59,20 @@ public class MainActivity extends AppCompatActivity {
     public void Reset(View v){
         mp.seekTo(0);
     }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
 }
